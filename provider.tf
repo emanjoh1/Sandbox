6c1fdde7a9c1d431344761d25e91d775c381a505
Emile Manjoh <babstoure@emiles-mbp.mynetworksettings.com>
2024-11-24 16:11:09 -0500

    add provider

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  cloud { 
+    
+    organization = "Babs45" 
+
+    workspaces { 
+      name = "sandbox" 
+    } 
+  } 
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
